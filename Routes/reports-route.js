3db90d29ce5973697a7857f69c3867eb3508ec74
MohamedAbdeltwab401 <118615015+mohamedsayed62@users.noreply.github.com>
2026-08-07 19:35:03 +0300

    Add downloadExcelAdmin route and update imports

--- Routes/reports-route.js
+++ Routes/reports-route.js
@@ -1,7 +1,7 @@
 const app = require('express');
 const router = app.Router();
 
-const {showReports, downloadExcel, showAdminReports}  = require('../controller/reports-controller');
+const {showReports, downloadExcel, showAdminReports, downloadExcelAdmin}  = require('../controller/reports-controller');
 const verifyToken = require('../middleware/verify-token-middleware');
 
 const { validateUser }  = require('../middleware/validator-middleware');
@@ -13,6 +13,9 @@ router.route('/admin')
   .post(verifyToken, showAdminReports);
 
 router.route('/download')
-  .post(downloadExcel)
+  .post(downloadExcel);
+
+router.route('/download/admin')
+  .get(verifyToken, downloadExcelAdmin);
 
 module.exports = router;
