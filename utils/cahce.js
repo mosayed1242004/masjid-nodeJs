14992b0231ebb94b039d83b0ae803672fcfb3e1a
MohamedAbdeltwab401 <118615015+mohamedsayed62@users.noreply.github.com>
2026-08-22 00:02:51 +0300

    Modify cache TTL and implement delByPrefix method
    Updated cache expiration time and added delByPrefix method.

--- utils/cahce.js
+++ utils/cahce.js
@@ -1,8 +1,16 @@
 const NodeCache = require("node-cache");
 
 const cache = new NodeCache({
-    stdTTL: 120, // البيانات تعيش 60 ثانية
+    stdTTL: 60, // البيانات تعيش 60 ثانية
     checkperiod: 120
 });
+
+cache.delByPrefix = function (prefix) {
+    const matchedKeys = this.keys().filter(key => key.startsWith(prefix));
+    if (matchedKeys.length > 0) {
+        this.del(matchedKeys);
+    }
+    return matchedKeys.length;
+};
 
 module.exports = cache;
